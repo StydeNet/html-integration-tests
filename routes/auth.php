dbbd8ca2abea86d9fe9e8b74e7a14b0fd9702e1a
Duilio Palacios <duilio@styde.net>
2017-09-02 16:45:16 +0100

    fix routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,19 +4,19 @@ Route::group(['prefix' => 'auth'], function () {
     Route::get('login', 'Auth\AuthController@getLogin');
 
     // Authentication routes...
-    Route::get('login', 'Auth\AuthController@getLogin');
-    Route::post('login', 'Auth\AuthController@postLogin');
-    Route::get('logout', 'Auth\AuthController@getLogout');
+    Route::get('login', 'Auth\LoginController@getLogin');
+    Route::post('login', 'Auth\LoginController@postLogin');
+    Route::get('logout', 'Auth\LoginController@getLogout');
 
     // Registration routes...
-    Route::get('register', 'Auth\AuthController@getRegister');
-    Route::post('register', 'Auth\AuthController@postRegister');
+    Route::get('register', 'Auth\RegisterController@getRegister');
+    Route::post('register', 'Auth\RegisterController@postRegister');
 
     // Password reset link request routes...
-    Route::get('password/email', 'Auth\PasswordController@getEmail');
-    Route::post('password/email', 'Auth\PasswordController@postEmail');
+    Route::get('password/email', 'Auth\ForgotPasswordController@getEmail');
+    Route::post('password/email', 'Auth\ForgotPasswordController@postEmail');
 
     // Password reset routes...
-    Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-    Route::post('password/reset', 'Auth\PasswordController@postReset');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@getReset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@postReset');
 });
